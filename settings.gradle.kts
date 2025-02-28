8549938a65db7e108f4bd1d76a336669b0b6be4f
junhee-ko <junheee.ko@gmail.com>
2025-02-28 13:22:16 +0900

    initialize project setting
    - set gradle multi module
       - common
       - service
       - service:article
       - service:comment
       - service:view
       - service:like
       - service:hot-article
       - service:article-read

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
 rootProject.name = "board"
+
+include(
+    "common",
+    "service",
+    "service:article",
+    "service:comment",
+    "service:view",
+    "service:like",
+    "service:hot-article",
+    "service:article-read"
+)
