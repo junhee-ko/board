cd6f2097e8b5e97997dbf57cd85a38fde1b38002
junhee-ko <junheee.ko@gmail.com>
2025-02-28 14:41:00 +0900

    update project setting > gradle files

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,17 @@
 plugins {
     kotlin("jvm") version "1.9.25"
     kotlin("plugin.spring") version "1.9.25"
-    id("io.spring.dependency-management") version "1.1.7"
+    id("org.springframework.boot") version "3.3.2"
+    id("io.spring.dependency-management") version "1.1.6"
 }
 
 group = "me.jko"
 version = "0.0.1-SNAPSHOT"
 
-subprojects {
-    apply(plugin = "kotlin")
+allprojects {
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+    apply(plugin = "org.jetbrains.kotlin.plugin.spring")
+    apply(plugin = "org.springframework.boot")
     apply(plugin = "io.spring.dependency-management")
 
     repositories {
--- service/hot-article/build.gradle.kts
+++ service/hot-article/build.gradle.kts
@@ -1,9 +1,3 @@
-plugins {
-    kotlin("jvm") version "1.9.25"
-    kotlin("plugin.spring") version "1.9.25"
-    id("org.springframework.boot") version "3.2.3"
-}
-
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
 }
